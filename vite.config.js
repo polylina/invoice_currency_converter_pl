0530504b4f468c4d6d965ea302400790bad7f0fa
Polina Gurina <76707268+polylina@users.noreply.github.com>
2026-03-08 22:32:08 +0100

    FIx build and version

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: "./",
   build: {
-    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        sidebar: resolve(__dirname, 'sidebar.html'),
+        sidebar: resolve(__dirname, "sidebar.html"),
       },
       output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
       },
     },
   },
-})
+});
